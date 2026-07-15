1cf0fa071ab4fd2b2b7ee73e5ef2ec0184d7b6df
Daniel Matthews <d.matthews@ucl.ac.uk>
2026-07-15 13:11:23 +0100

    Template installation overlay and kustomize calico custom resources

--- bundles/calico/run.sh
+++ bundles/calico/run.sh
@@ -4,29 +4,24 @@ set -ex
 
 K3S_MANIFEST_DIR="/var/lib/rancher/k3s/server/manifests/"
 
+# get the calico CONFIG from the config and set them to the CONFIG variable if they exist
+_config=$(kairos-agent config get "calico.extraConfig | @json" | tr -d '\n')
+
+# Remove the quotes wrapping the value.
+_config=${_config:1:${#_config}-2}
+if [ "$_config" != "null" ]; then
+    CONFIG=$_config
+fi
+
+if [ -n "$CONFIG" ]; then
+  # template the calico installation overlay with the CONFIG from the config
+  sed -i "s/@CONFIG@/${CONFIG}/g" "assets/overlays/installation.yaml"
+
+  # kustomize the calico custom resources
+  cp assets/custom-resources.yaml assets/overlays/custom-resources.yaml
+  kubectl kustomize assets/overlays > assets/custom-resources.yaml
+fi
+
+# copy the calico manifests to the k3s manifest directory
 mkdir -p $K3S_MANIFEST_DIR
-
-# renovate: depName=tigera-operator repoUrl=https://docs.tigera.io/calico/charts
-VERSION="v3.32.1"
-
-_version=$(kairos-agent config get "calico.version" | tr -d '\n')
-if [ "$_version" != "null" ]; then
-    VERSION=$_version
-fi
-
-MTU=1450
-_mtu=$(kairos-agent config get "calico.mtu" | tr -d '\n')
-if [ "$_mtu" != "null" ]; then
-  MTU=$_mtu
-fi
-
-# download the calico manifests
-v1_crd_url="https://raw.githubusercontent.com/projectcalico/calico/${VERSION}/manifests/v1_crd_projectcalico_org.yaml"
-curl -L -o assets/v1_crd_projectcalico_org.yaml "${v1_crd_url}"
-tigera_operator_url="https://raw.githubusercontent.com/projectcalico/calico/${VERSION}/manifests/tigera-operator.yaml"
-curl -L -o assets/tigera-operator.yaml "${tigera_operator_url}"
-calico_cr_url="https://raw.githubusercontent.com/projectcalico/calico/${VERSION}/manifests/custom-resources.yaml"
-curl -L -o assets/custom-resources.yaml "${calico_cr_url}"
-
-# patch the cr manifest to include mtu under calicoNetwork
-sed -i "s/calicoNetwork:/calicoNetwork:\n    mtu: ${MTU}/" assets/custom-resources.yaml
+cp -f assets/* $K3S_MANIFEST_DIR
